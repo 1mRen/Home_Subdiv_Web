0aa3c513da726ece16121199f8ad9bd574487d57
xx827 <xx827@CCSLAB530U22>
2025-02-01 12:41:35 +0800

    Add project files.

---
+++ Home_Subdiv_Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Home_Subdiv_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home_Subdiv_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Home_Subdiv_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Home_Subdiv_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
