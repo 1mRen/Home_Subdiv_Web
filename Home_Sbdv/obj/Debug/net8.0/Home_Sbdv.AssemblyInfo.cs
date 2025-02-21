3a4b8d5a46675199eb2c19437b8bb277e6667338
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-02-21 12:12:42 +0800

    stage -> 1

--- Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
+++ Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3234927d5bc57ee7da87bb86a259465311511d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f7cef4382b15a9fe96717c87c30e7a05a8e5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
