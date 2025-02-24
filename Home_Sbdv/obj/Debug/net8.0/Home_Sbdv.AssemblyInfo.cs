b325fa5de3d753c06b8dcc3a38e734daaedee07c
Andry <145820299+spOke426@users.noreply.github.com>
2025-02-25 05:07:48 +0800

    Fixed Commit

--- Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
+++ Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab592b41b4d2a60c78efc41c6e470df486807a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d726474697b907cefb463886e0f0a566e47a4180")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
