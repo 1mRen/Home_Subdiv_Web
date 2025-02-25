1a3b74bb5c72677639c589dfc55f30ba50752454
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-02-26 00:52:24 +0800

    ui -> change-landing-page

--- Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
+++ Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc8fd703269434ace0173594f1935a992d8ff71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb17f44fef556b0074bcb98a9d5af5ebd643ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
