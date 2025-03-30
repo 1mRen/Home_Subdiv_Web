1e9a94f64370d79740855999ab26f34eb350d3e0
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-03-30 20:12:02 +0800

    feat: test

--- Home_Sbdv/wwwroot/js/site.js
+++ Home_Sbdv/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const modeToggleBtn = document.getElementById('modeToggleBtn');
+const modeIcon = document.getElementById('modeIcon');
+
+// Check for saved mode preference
+const savedMode = localStorage.getItem('theme');
+if (savedMode === 'night') {
+  document.body.classList.add('night-mode');
+  modeIcon.classList.replace('bi-sun', 'bi-moon');
+}
+
+modeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('night-mode');
+
+  if (document.body.classList.contains('night-mode')) {
+    modeIcon.classList.replace('bi-sun', 'bi-moon');
+    localStorage.setItem('theme', 'night');
+  } else {
+    modeIcon.classList.replace('bi-moon', 'bi-sun');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
