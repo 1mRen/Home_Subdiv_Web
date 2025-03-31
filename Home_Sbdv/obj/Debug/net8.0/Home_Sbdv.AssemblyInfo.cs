f47cf503c59cf47ea12dff65bd77e5d0d3326e5e
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-03-31 14:00:52 +0800

    palma feat : admin-page, functionality, dashboard-fix, hover-fix

--- Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
+++ Home_Sbdv/obj/Debug/net8.0/Home_Sbdv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def047ec42ad07a3fcd5c25eedac0b16b436b380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40978fe00b5786842662a6994ff7003d1b0a0818")]
 [assembly: System.Reflection.AssemblyProductAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Home_Sbdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
