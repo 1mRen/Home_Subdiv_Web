a6a92eb28a7bf402efd7b5ef815a752c69594162
Marc <mmarclawrence@gmail.com>
2025-04-05 02:31:57 +0800

    Add DashboardController and update various views
    - Introduced DashboardController with Index and SecurePage actions
    - Updated Home_Sbdv.csproj to manage .editorconfig correctly
    - Modified SecurePage.cshtml to set model and include partial view
    - Corrected asp-controller attribute in ListUsers.cshtml
    - Made minor formatting change in appsettings.Development.json

---
+++ Home_Sbdv/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Home_Sbdv.Data;
+using Home_Sbdv.Entities;
+
+namespace Home_Sbdv.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> SecurePage()
+        {
+            ViewBag.Name = HttpContext.User.Identity?.Name ?? "Guest";
+            ViewBag.Role = HttpContext.User.FindFirst(ClaimTypes.Role)?.Value ?? "HomeOwner";
+
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+    }
+}
