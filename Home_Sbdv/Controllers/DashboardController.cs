30ec70af5fa29829f803edd39b4f04ba37535401
Marc <mmarclawrence@gmail.com>
2025-04-05 01:36:45 +0800

    wtf

---
+++ Home_Sbdv/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home_Sbdv.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult SecurePage()
+        {
+            ViewBag.Name = HttpContext.User.Identity?.Name ?? "Guest";
+            ViewBag.Role = HttpContext.User.FindFirst(ClaimTypes.Role)?.Value ?? "HomeOwner";
+            return View();
+        }
+    }
+}
