cde5e909cf118e5dd492097a732bf39724ee4adc
Jairus Azer Andrade <jairusandrade05@gmail.com>
2025-05-10 20:16:17 +0800

    updated homepage design

--- Home_Sbdv/Migrations/20250510115235_HomeSubdb.Designer.cs
+++ Home_Sbdv/Migrations/20250510115235_HomeSubdb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Home_Sbdv.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Home_Sbdv.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250510115235_HomeSubdb")]
+    partial class HomeSubdb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
